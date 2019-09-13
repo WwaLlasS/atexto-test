23717d644bf7c696b191ff58a0133ec8a2aadd4f
Williams Rincon <wwallass.0310@gmail.com>
2019-09-13 01:17:49 -0500

    crud is done

--- back-end/controllers/audioRecords.js
+++ back-end/controllers/audioRecords.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const AudioRecordings = require('../models/audioRecordings.model')
+const fs = require('fs')
 
 router.route('/').get((request, response) => {
   AudioRecordings.find()
@@ -12,27 +13,64 @@ router.route('/add').post((request, response) => {
     name,
     data
   } = request.body
-  const audioRecord = new AudioRecordings({name, data})
+  const url = `/audio/${name}.ogg`
+  const audioRecord = new AudioRecordings({name, data, url})
   audioRecord.save()
-    .then(() => response.json('Audio Added successfully'))
+    .then(file => {
+      try {
+        fs.writeFileSync(`${__dirname}/../media/audio/${name}.ogg`, Buffer.from(data.replace('data:audio/ogg; codecs=opus;base64,', ''), 'base64'))
+      } catch (err){
+        console.log(err)
+        return response.status(500).end()  
+      }
+      return response.json('Audio Added successfully')
+    })
     .catch(err => response.status(400).json(`Error: ${err}`))
 })
 
 router.route('/delete').post((request, response) => {
-  const id = request.body.id
-  AudioRecordings.deleteOne({"_id": id})
-    .then(() => response.json('Audio Deleted'))
-    .catch(err => response.status(400).json(`Error: ${err}`))
+  const {
+    id,
+    name
+  } = request.body
+  let url = '/'
+  let pathArray = __dirname.split('/')
+  pathArray.pop()
+  pathArray.shift()
+  pathArray.forEach(item => {
+    url += item + '/' 
+  })
+  try {
+    fs.unlinkSync(`${url}media/audio/${name}.ogg`)
+    AudioRecordings.deleteOne({"_id": id})
+      .then(() => response.json('Audio Deleted'))
+      .catch(err => response.status(400).json(`Error: ${err}`))
+  } catch (err) {
+    return response.json({error: err})
+  }
 })
 
 router.route('/edit').post((request, response) => {
   const {
     id,
-    name
+    oldName,
+    newName
   } = request.body
-  AudioRecordings.updateOne({ _id: id }, { name })
-    .then(() => response.json('Audio Updated'))
-    .catch(err => response.status(400).json(`Error: ${err}`))
+  try {
+    let url = '/'
+    let pathArray = __dirname.split('/')
+    pathArray.pop()
+    pathArray.shift()
+    pathArray.forEach(item => {
+      url += item + '/' 
+    })
+    fs.renameSync(`${url}media/audio/${oldName}.ogg`, `${url}media/audio/${newName}.ogg`)
+    AudioRecordings.updateOne({ _id: id }, { name: newName })
+      .then(() => response.json('Audio Updated'))
+      .catch(err => response.status(400).json(`Error: ${err}`))
+  } catch (err) {
+    return response.json(err)
+  }
 })
 
 module.exports = router
